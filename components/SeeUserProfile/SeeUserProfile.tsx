00bd53ea470a0d854ba2e1a7cf19bcff5c615a58
Misael <misa_30122@hotmail.com>
2022-05-30 19:58:06 -0700

    User Profile Design Changes

--- components/SeeUserProfile/SeeUserProfile.tsx
+++ components/SeeUserProfile/SeeUserProfile.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { motion } from 'framer-motion';
 import PetCardDetail from "../Cards/PetCardDetail";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDog } from "@fortawesome/free-solid-svg-icons";
+import { faArrowCircleLeft, faArrowCircleRight, faDice, faDog, faHeartCirclePlus, faImages, faPlay, faStop } from "@fortawesome/free-solid-svg-icons";
 import { useRouter } from "next/router";
 
 interface User {
@@ -51,7 +51,38 @@ function SeeUserProfile({ data }: any) {
     const [user] = useState<User>(data.user as User);
     const [userProfile] = useState<UserProfile>(data.userProfile as UserProfile);
     const [pets] = useState<Pet[]>(data.pets as Pet[]);
+    const [isPlaying, setIsPlaying] = useState<boolean>(true);
+    const [currentImageURL, setCurrentImageURL] = useState<string>(data.userProfile.imagesURL[0] ? data.userProfile.imagesURL[0] : '');
+    const [currentIndex, setCurrentIndex] = useState<number>(0);
     const router = useRouter();
+
+    const variants = {
+        play: {
+            opacity: 1,
+            translateX: [0, 100, -100, 0],
+            rotateZ: [0, 100, -100, 0],
+            transition: {
+                ease: "easeInOut",
+                delay: 0.2,
+                repeat: Infinity,
+                repeatDelay: 1,
+                duration: 2
+            }
+        },
+        stop: { opacity: 1, translateX: 0, rotateZ: 0 },
+    }
+
+    const handleOnClickPreviousImage = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - 1);
+        }
+    }
+
+    const handleOnClickNextImage = () => {
+        if (userProfile.imagesURL.length - 1 > currentIndex) {
+            setCurrentIndex(currentIndex + 1);
+        }
+    }
 
     return (
         <div>
@@ -147,6 +178,75 @@ function SeeUserProfile({ data }: any) {
                             </motion.div>
                         ))
                     }
+                </div>
+            </div>
+
+            <div className="mt-20 w-[80%] mx-auto text-center">
+                <h1 className="mb-2"><FontAwesomeIcon icon={faDice} /> Hobbies</h1>
+                <hr className="border-black" />
+
+                <div className="mt-10 flex flex-row justify-evenly">
+                    {
+                        userProfile?.hobbies.map((element: string, index: number) => (
+                            <motion.div
+                                whileInView={isPlaying ? 'play' : 'stop'}
+                                variants={variants}
+                                key={index} className='card w-80 h-20 flex items-center justify-center'>
+                                <h4>{element}</h4>
+                            </motion.div>
+                        ))
+                    }
+                </div>
+                <div className="flex flex-row justify-center mt-20">
+                    <button onClick={() => setIsPlaying(!isPlaying)} className="btn-dark w-32" type="button">
+                        {isPlaying ? <span><FontAwesomeIcon icon={faStop} /> Stop</span> : <span><FontAwesomeIcon icon={faPlay} /> Play</span>}
+                    </button>
+                </div>
+            </div>
+
+            <div className="mt-40 w-[80%] mx-auto text-center mb-2">
+                <h1 className="mb-2"><FontAwesomeIcon icon={faImages} /> Favorites Images :)</h1>
+                <hr className="border-black" />
+            </div>
+
+            <div className="bg-black">
+                <div className="flex flex-row w-4/5 mx-auto h-[70vh]">
+                    <div className="w-2/4 flex items-center justify-center">
+                        <h1 className="text-white text-[7rem]">
+                            These Are the Great Images That I Like <FontAwesomeIcon icon={faHeartCirclePlus} className='text-red-500' />
+                        </h1>
+                    </div>
+
+                    <div className="w-2/4 flex items-center justify-center">
+                        <div className="w-full">
+                            {
+                                <motion.img
+                                    initial={{
+                                        opacity: 0
+                                    }}
+                                    whileInView={{
+                                        opacity: 1,
+                                        transition: {
+                                            duration: 1
+                                        }
+                                    }}
+                                    viewport={{
+                                        once: false
+                                    }}
+                                    layout transition={{
+                                        type: 'spring'
+                                    }}
+                                    src={userProfile.imagesURL[currentIndex]}
+                                    className='mt-20 max-h-[25rem] mx-auto' />
+                            }
+                            <div className="flex flex-row justify-evenly mt-10 items-center">
+                                <button disabled={currentIndex === 0} onClick={handleOnClickPreviousImage} className="w-20 btn-dark" type="button"><FontAwesomeIcon icon={faArrowCircleLeft} /></button>
+                                <h5 className="text-white">Image {currentIndex + 1} Out Of {userProfile.imagesURL.length}</h5>
+                                <button disabled={currentIndex === userProfile.imagesURL.length - 1} onClick={handleOnClickNextImage} className="w-20 btn-dark" type="button"> <FontAwesomeIcon icon={faArrowCircleRight} /></button>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
